929466c0e1cc1420b4e70ae8db0ca4577f18403d
Sam Billingham <sambillie.sb@gmail.com>
2014-04-16 22:16:37 +0100

    more setup

---
+++ web/gulpfile.js
@@ -0,0 +1,48 @@
+// gulp
+var gulp = require('gulp');
+
+// Plugins
+var jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber');
+
+// Lint Task
+gulp.task('lint', function() {
+    gulp.src('public/js/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    gulp.src('sass/{*.sass,*.scss}')
+        .pipe(plumber())
+        .pipe(sass({ style: 'compressed', lineNumbers : true }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('public/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    gulp.src(['public/js/main.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('public/js/dist'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify({outSourceMap: true}))
+        .pipe(gulp.dest('public/js/dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('public/js/*.js', ['lint', 'scripts']);
+    gulp.watch('sass/*/{*.sass,*.scss}', ['sass']);
+});
+
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
