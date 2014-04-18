1a0fa440cf6bee0e746eb31d69cee4b45c6082a4
Sam Billingham <sambillie.sb@gmail.com>
2014-04-18 23:33:49 +0100

    basic spotify search

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,14 +1,14 @@
 // gulp
-var gulp = require('gulp');
-
-// Plugins
-var jshint = require('gulp-jshint'),
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    jshint = require('gulp-jshint'),
     sass = require('gulp-ruby-sass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    source = require('vinyl-source-stream');
 
 // Lint
 gulp.task('lint', function() {
@@ -30,7 +30,7 @@ gulp.task('sass', function() {
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    gulp.src(['public/js/main.js'])
+    gulp.src(['public/js/vendor/jquery-1.11.0.min.js','public/js/main.js'])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('public/js/dist'))
         .pipe(rename('main.min.js'))
@@ -40,7 +40,7 @@ gulp.task('scripts', function() {
 
 // Watch Files
 gulp.task('watch', function() {
-    gulp.watch('public/js/*.js', ['lint', 'scripts']);
+    gulp.watch('public/js/**/*.js', ['lint', 'scripts']);
     gulp.watch('sass/**/{*.sass,*.scss}', ['sass']);
 });
 
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -18,6 +18,18 @@ $(function () {
         bindUiActions : function () {
             var self = this;
 
+            $('.search-btn').on('click', function() {
+                var searchParams = encodeURI( $('.search-input').val() );
+
+                $.ajax({
+                    url: 'https://ws.spotify.com/search/1/track.json?q=' + searchParams,
+                    type: "GET",
+                    success: function(data) {
+                        self.displayTracks(data);
+                    }
+                });
+            });
+
             $('.send-track').on('click', function() {
                 self.socket.emit('new-track', {
                     uri: 'spotify:track:21OwGeh8c5HUWhJ1TmKDUa'
@@ -33,9 +45,27 @@ $(function () {
         socketEvents : function() {
             var self = this;
 
-            this.socket.on('current-status', function(){
-                console.log('pong');
+            this.socket.on('current-status', function(data){
+                console.log('Current Status: ', data);
             });
+        },
+
+        displayTracks: function(data){
+            var ammountOfTracks,
+                tracks = data.tracks;                
+
+            console.log(data);
+            if( tracks.length > 10 ){
+                ammountOfTracks = 10;
+            } else {
+                ammountOfTracks = tracks.length;
+            }
+
+            for (var i = ammountOfTracks - 1; i >= 0; i--) {
+                console.log(tracks[i].name);
+                $('<li class="track">'+ tracks[i].name + ' - '+ tracks[i].artists[0].name + '</li>').hide().appendTo('.results > ul').fadeIn(500);
+            }
+
         }
     };
 
